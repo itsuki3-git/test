33f4a10facbab3bb293d24e47b8801ae40ae869c
itsuki3-git <itsuki-umi-daisuki@ezweb.ne.jp>
2026-08-20 20:41:40 +0900

    Update test.py

--- test.py
+++ test.py
@@ -401,4 +401,6 @@ def main(page: ft.Page):
 
 
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.AppView.WEB_BROWSER)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    ft.app(target=main, host="0.0.0.0", view=ft.AppView.WEB_BROWSER, port=port)
